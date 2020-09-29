e6afd9aabdbe3b724db158f3db0daf00e6bba180
dnsrocha <drocha@gmail.com>
2020-09-28 22:17:09 -0700

    Wave 3 - done

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,14 +6,28 @@ require_relative 'card'
 class Deck
 
   def initialize
-
+    valid_value = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    valid_suit = [:hearts, :spades, :clubs, :diamonds]
+    @deck = []
+    valid_value.each do |value|
+      valid_suit.each do |suit|
+        card = Card.new(value,suit)
+        @deck << card
+      end
+    end
   end
 
   def draw
     # returns a card
+    return @deck.shift
   end
 
   def shuffle
     # shuffles the deck
+    return @deck.shuffle
+  end
+
+  def count
+    return @deck.length
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,4 +6,30 @@ describe Deck do
     # 1.  Expected Behavior
     # 2.  Edge cases
 
+  before do
+    @deck = Deck.new
+  end
+
+  it "can be instantiated" do
+    expect(@deck).must_be_instance_of Deck
+  end
+
+  it "should have 52 cards" do
+    expect(@deck.count).must_equal 52
+  end
+
+  it "can draw a card" do
+    expect(@deck.draw).must_be_instance_of Card
+  end
+
+  it "deck can be shuffled" do
+    expect(@deck.shuffle).must_be_instance_of Array
+  end
+
+  it "has a count method" do
+    expect(@deck).must_respond_to :count
+    expect(@deck.count).must_be_instance_of Integer
+    expect(@deck.count).must_equal 52
+  end
+
 end
