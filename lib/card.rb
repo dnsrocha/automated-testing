123b9d66513a5b0990289ca0c4abab42e879ca97
dnsrocha <drocha@gmail.com>
2020-09-28 21:09:33 -0700

    Wave 1 - done

--- lib/card.rb
+++ lib/card.rb
@@ -7,12 +7,21 @@ class Card
   def initialize(value, suit)
     @value = value
     @suit = suit
-    
 
+    if value < 1 || value > 13
+      raise ArgumentError.new("Value must be between 1 and 13")
+    end
+
+    valid = [:hearts, :spades, :clubs, :diamonds]
+    unless valid.include?(suit)
+      raise ArgumentError.new("Suit must be Hearts, Spades, Clubs or Diamonds")
+    end
   end
 
   def to_s
     return "#{value} of #{suit.to_s}"
   end
+
+
 
 end
